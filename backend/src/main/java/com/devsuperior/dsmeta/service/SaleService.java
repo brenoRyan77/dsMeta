32fde838f0501c4b7790ec13c96182a39feb4d77
bryan <bryan@indracompany.com>
2022-07-13 13:47:33 -0300

    fix: melhorando a consulta de vendas

--- backend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -14,7 +18,13 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> listaVendas() {
-		return repository.findAll();
+	public Page<Sale> listaVendas(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate dataAtual = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? dataAtual.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? dataAtual : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 	}
 }
